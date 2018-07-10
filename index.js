3950ea99c171369fbd681007bd03b87eecd0e8cf
KimJungChul <kjc5443@gmail.com>
2018-07-10 23:55:29 +0900

    update comment index.js

--- index.js
+++ index.js
@@ -6,19 +6,17 @@ var Scanner = require('./Scanner');
 var UI = require('./UI');
 
 
-// Configure Robotjs
+// Robotjs를 설정합니다.
 robot.setMouseDelay(1);
 
 
-// Initialize Game
+// 게임을 초기화합니다.
 GameManipulator.findGamePosition();
 
 
-// Check for found game
+// 게임을 찾습니다.
 if (GameManipulator.offset) {
-  // Uncomment this line to debug the
-  // starting point of sensor (Check if it's detecting it correcly)
-
+  // 센서의 시작점(감지가 올바르게 되는지)을 디버그하기 위해서는 다음의 라인의 주석을 해제합니다.
   // robot.moveMouse(GameManipulator.offset[0]+GameManipulator.sensors[0].offset[0],
   //    GameManipulator.offset[1] + GameManipulator.sensors[0].offset[1]);
 
@@ -29,20 +27,21 @@ if (GameManipulator.offset) {
 }
 
 
-// Initialize UI
+// UI 초기화
 UI.init(GameManipulator, Learner);
 
 
-// Init Learner
+// Learner 초기화
+// 유전알고리즘(Genetic Algorithm)와 신경망(Neural Network))
 Learner.init(GameManipulator, UI, 12, 4, 0.2);
 
 
-// Start reading game state and sensors
+// 게임의 상태와 센서를 읽어 들이는 것을 시작합니다.
 setInterval(GameManipulator.readSensors, 40);
 setInterval(GameManipulator.readGameState, 200);
 
 
-// Start game (Example of API usage)
+// 게임 시작 (API 사용법의 예제)
 /*
 function startGame () {
   var game = Math.round(Math.random() * 100);
